03e140113fe35048533b5d4bee343345c9b4f501
just-jhorge <sarpongk247@gmail.com>
2022-08-11 12:50:36 +0000

    navbar improvements made

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,7 +32,7 @@ const Navbar = () => {
                     <div
                         className={`${
                             isOpen ? "hidden" : ""
-                        } text-4xl cursor-pointer text-ash`}
+                        } text-4xl cursor-pointer text-ash hover:text-white transition-colors`}
                         onClick={() => setIsOpen(!isOpen)}
                     >
                         <RiMenu3Fill />
@@ -45,33 +45,33 @@ const Navbar = () => {
                     transition={{ duration: 2 }}
                     className={`${
                         isOpen ? "fixed" : "hidden"
-                    } bg-white h-full w-full z-50 shadow-lg flex flex-col-reverse justify-center items-center md:grid md:grid-cols-2`}
+                    } bg-white h-full w-full z-50 shadow-lg top-0 flex flex-col-reverse md:flex-row items-center justify-center`}
                 >
-                    <div
-                        className="text-white
-                     md:grid md:grid-rows-2"
-                    >
-                        <div className="bg-green-200">
+                    <div className="min-h-[30%] md:h-full w-full text-white flex flex-col items-center justify-center">
+                        <div className="h-full w-full">
                             <img
                                 src={require("../assets/pattern.png")}
                                 alt="Pattern"
                                 className="h-full w-full"
                             />
                         </div>
-                        <div className="hidden bg-blue-500 p-20 md:flex items-end justify-between leading-loose font-Josefin">
+                        <div className="bg-blue-500 h-full w-full hidden py-14 px-20 md:flex items-end justify-between leading-loose font-Josefin">
                             <div className="flex flex-col space-y-5">
                                 <h3 className="capitalize font-bold text-4xl">
                                     stay in touch.
                                 </h3>
-                                <a href="mailto:sarpongk247@gmail.com">
+                                <a
+                                    className="text-lg"
+                                    href="mailto:sarpongk247@gmail.com"
+                                >
                                     sarpongk247@gmail
                                 </a>
-                                <p>+233 241 385 150</p>
-                                <p>Kumasi, Ghana</p>
+                                <p className="text-lg">+233 241 385 150</p>
+                                <p className="text-lg">Kumasi, Ghana</p>
                             </div>
                             <div className="flex flex-col space-y-5">
                                 <h1 className="text-5xl font-bold">GAS.</h1>
-                                <p>Let's get creative</p>
+                                <p className="text-lg">Let's get creative</p>
                                 <div className="flex items-center justify-start space-x-5 text-2xl text-white">
                                     <RiFacebookFill />
                                     <RiGithubFill />
@@ -80,21 +80,21 @@ const Navbar = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="relative flex items-center justify-center text-ash px-40 py-5">
-                        <nav className="flex flex-col items-start justify-center">
+                    <div className="h-full w-full bg-white relative flex items-center justify-center">
+                        <nav className="flex flex-col items-start justify-center text-ash">
                             {homeLinks.map((link) => (
                                 <Link
                                     to={`${link.to}`}
-                                    className="text-4xl font-Josefin font-bold my-10 capitalize hover:text-blue-400 transition-colors"
+                                    className="text-3xl md:text-5xl font-Josefin font-semibold md:font-bold my-4 md:my-10 capitalize hover:text-blue-400 transition-colors"
                                     key={link.name}
                                     onClick={() => setIsOpen(false)}
                                 >
-                                    {link.name}.
+                                    {link.name}
                                 </Link>
                             ))}
                         </nav>
                         <div
-                            className="absolute right-10 top-5 text-4xl cursor-pointer"
+                            className="absolute text-4xl right-5 md:right-10 top-5 cursor-pointer text-ash"
                             onClick={() => setIsOpen(false)}
                         >
                             <RiCloseFill />
